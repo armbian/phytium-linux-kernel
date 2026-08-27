f057c04ccfe1da58fbd14a100592e79e5934f0ca
huangjie1663 <huangjie1663@phytium.com.cn>
2026-08-27 10:25:41 +0800

    dma: phytium-gdmac: bump driver version to 1.0.6
    Version bump accompanying the terminate_all double-free fix
    (c25f0bfa9d1d).
    
    Signed-off-by: huangjie1663 <huangjie1663@phytium.com.cn>

--- drivers/dma/phytium/phytium-gdmac.c
+++ drivers/dma/phytium/phytium-gdmac.c
@@ -31,7 +31,7 @@
 #include <asm/barrier.h>
 #include "phytium-gdmac.h"
 
-#define PHYTIUM_GDMA_DRIVER_VERSION	"1.0.5"
+#define PHYTIUM_GDMA_DRIVER_VERSION	"1.0.6"
 
 static inline struct phytium_gdma_device *to_gdma_device(struct dma_chan *chan)
 {
